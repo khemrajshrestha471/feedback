fc945d83206541e229b03486c1b91f5fee815cf2
Khemraj Shrestha <khemrajshrestha471@gmail.com>
2024-09-28 12:53:42 +0545

    signup route competed with route

--- src/model/User.ts
+++ src/model/User.ts
@@ -68,5 +68,3 @@ const UserSchema: Schema<User> = new Schema({
 const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema);
 
 export default UserModel;
-
-// 36:19
